df32422f759c71e89ca5744beba78d6544630120
Mohammed Hicham Benhabib <mobenhab@z4r9p4.42lyon.fr>
2026-02-13 12:59:13 +0100

    py09 ex0 finish

---
+++ python09/ex0/space_station.py
@@ -0,0 +1,124 @@
+from pydantic import BaseModel, ValidationError, field_validator
+from typing import Optional
+from datetime import datetime
+
+class SpaceStation(BaseModel):
+    station_id: str
+    name: str
+    crew_size: int
+    power_level: float
+    oxygen_level: float
+    last_maintenance: datetime
+    is_operational: bool
+    notes: Optional[str]
+
+    @field_validator("station_id")
+    @classmethod
+    def check_id(cls, station_id: str) -> str:
+        if len(station_id) < 3 or len(station_id) > 10:
+            raise ValueError("ID should be less than 10 or more than 3")
+        return station_id
+    
+    @field_validator("name")
+    @classmethod
+    def check_name(cls, name: str) -> str:
+        if len(name) < 1 or len(name) > 50:
+            raise ValueError("name should be more than 1 or less thane 50")
+        return name
+    
+    @field_validator("crew_size")
+    @classmethod
+    def crew_size_is_valid(cls, crew_size: int) -> int:
+        if crew_size < 1 or crew_size > 20:
+            raise ValueError("crew_size must be between 1 and 20")
+        return crew_size
+    
+    @field_validator("power_level")
+    @classmethod
+    def check_power(cls, power_level: float) -> float:
+        if power_level < 0.0 or power_level > 100.0:
+            raise ValueError("power_level should be less than 100.0 or more than 0.0")
+        return power_level
+    
+    @field_validator("oxygen_level")
+    @classmethod
+    def check_oxygen(cls, oxygen_level: float) -> float:
+        if oxygen_level < 0.0 or oxygen_level > 100.0:
+            raise ValueError("oxygen_level should be less than 100.0 or more than 0.0")
+        return oxygen_level
+    
+    @field_validator("is_operational")
+    @classmethod
+    def check_operational(cls, is_operational: bool) -> bool:
+        if not isinstance(is_operational, bool):
+            raise ValueError("is_operational should be true or false")
+        return is_operational
+    
+    @field_validator("notes")
+    @classmethod
+    def check_notes(cls, notes: Optional[str]) -> Optional[str]:
+        if notes is not None and len(notes) > 200:
+            raise ValueError("notes should have max 200 characters")
+        return notes
+    
+
+def main():
+    print("Space Station Data Validation")
+    print("========================================")
+    try:
+        station1: SpaceStation = SpaceStation(
+            station_id=" ISS001",
+            name=" International Space Station",
+            crew_size=6,
+            power_level=85.5,
+            oxygen_level=92.3,
+            last_maintenance=datetime(2024, 5, 20, 14, 30),
+            is_operational=True,
+            notes=None
+        )
+        print("Valid station created:")
+        print(f"ID: {station1.station_id}")
+        print(f"Name: {station1.name}")
+        print(f"Crew: {station1.crew_size} people")
+        print(f"Power: {station1.power_level}%")
+        print(f"Oxygen: {station1.oxygen_level}%")
+        print("Status", end=" ")
+        if (station1.is_operational):
+            print("Operational")
+        else:
+            print("Not Operational")
+    except ValidationError as e:
+        print(f"Expected validation error: ")
+        print(e)
+    print()
+    print("========================================")
+    try:
+        station2: SpaceStation = SpaceStation(
+            station_id="hicham",
+            name=" International Space Station",
+            crew_size=80,
+            power_level=85.5,
+            oxygen_level=92.3,
+            last_maintenance=datetime(2024, 5, 20, 14, 30),
+            is_operational=False,
+            notes=None
+        )
+        print("Space Station Data Validation")
+        print("========================================")
+        print("Valid station created:")
+        print(f"ID: {station2.station_id}")
+        print(f"Name: {station2.name}")
+        print(f"Crew: {station2.crew_size} people")
+        print(f"Power: {station2.power_level}%")
+        print(f"Oxygen: {station2.oxygen_level}%")
+        print("Status", end=" ")
+        if (station2.is_operational):
+            print("Operational")
+        else:
+            print("Not Operational")
+    except ValidationError as e:
+        print(f"Expected validation error:")
+        print(e)
+
+if __name__ == "__main__":
+    main()
---
+++ python09/ex1/alien_contact.py
@@ -0,0 +1,69 @@
+from pydantic import ValidationError, model_validator, BaseModel, field_validator
+from datetime import datetime
+from typing import Optional
+
+class ContactType(BaseModel):
+    radio = "radio"
+    visual = "visual"
+    physical = "physical"
+    telepathic = "telepathic"
+
+class AlienContact(BaseModel):
+    contact_id: str
+    timestamp: datetime
+    location: str
+    contact_type: ContactType
+    signal_strength: float
+    duration_minutes: int
+    witness_count: int
+    message_received: Optional[str]
+    is_verified: bool
+
+    @field_validator('contact_id')
+    @classmethod
+    def check_contact(cls, contact_id: str) -> str:
+        if len(contact_id) < 5 or len(contact_id) > 15:
+            raise ValueError("contact_id must be less than 15 and more than 5")
+        return contact_id
+    
+    @field_validator('location')
+    @classmethod
+    def check_location(cls, location: str) -> str:
+        if len(location) < 3 or len(location) > 100:
+            raise ValueError("location must be less than 100 or more than 3")
+        return location
+    
+    @field_validator('signal_strenght')
+    @classmethod
+    def check_signal(cls, signal_strenght: float) -> float:
+        if signal_strenght < 0 or signal_strenght > 10.0:
+            raise ValueError("signal_strenght should be less than 10 and modre than 0")
+        return signal_strenght
+    
+    @field_validator('duration_minutes')
+    @classmethod
+    def check_duration(cls, duration_minutes: int) -> int:
+        if duration_minutes < 1 or duration_minutes > 1440:
+            raise ValueError("min duration 1min and max 1440min = 24h")
+        return duration_minutes
+    
+    @field_validator('witness_count')
+    @classmethod
+    def check_witness(cls, witness_count: int) -> int:
+        if witness_count < 0 or witness_count > 1000:
+            raise ValueError("min people 1 and max 100")
+        return witness_count
+    
+    @field_validator('message_received')
+    @classmethod
+    def check_message(cls, message_received: Optional[str]) -> Optional[str]:
+        if message_received is not None and len(message_received) > 500:
+            raise ValueError("max len for the mesaage is 500")
+        return message_received
+    
+    @field_validator('is_verified')
+    @classmethod
+    def check_verifiction(cls, is_verified: bool) -> bool:
+        if not isinstance(is_verified, bool):
+            raise ValueError("is_verified must be true or false")
+        return (is_verified)
