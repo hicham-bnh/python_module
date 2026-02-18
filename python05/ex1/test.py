ad327c26c9e57042b08264d3d5331581b940c388
Mohammed Hicham Benhabib <mobenhab@z4r9p4.42lyon.fr>
2026-02-18 10:39:33 +0100

    ex1 transaction stream finish

--- python05/ex1/test.py
+++ python05/ex1/test.py
@@ -36,19 +36,46 @@ class SensorStream(DataStream):
             if isinstance(item, str) and item.startswith("temp:"):
                 temp = float(item.split(":")[1])
                 break
-        self.processed_count+= len(result)
+        self.processed_count += len(result)
         if temp is None:
             return f"{len(result)} operations"
         return f"{len(result)} readings processed, avg temp: {temp}°C"
     
 
+class TransactionStream(DataStream):
+    def __init__(self, stream_id: str, stream_type: str):
+        super().__init__(stream_id, "Financial Data")
 
+    def filter_data(self, data_batch: List[Any]) -> List[Any]:
+        return [x for x in data_batch if isinstance(x, str)]
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        result: List[Any] = self.filter_data(data_batch)
+        transaction = 0
+        try:
+            for item in result:
+                if isinstance(item, str) and item.startswith("buy:"):
+                    transaction += int(item.split(":")[1])
+                elif isinstance(item, str) and item.startswith("sell:"):
+                    transaction -= int(item.split(":")[1])
+            self.processed_count += len(result)
+            if transaction > 0:
+                return f"{len(result)} operations, net flow: +{transaction} units"
+            return f"{len(result)}  operations, net flow: -{transaction} units"
+        except Exception as e:
+            return f"{e}"
 
 if __name__ == "__main__":
-    process_sensor = SensorStream(" SENSOR_001","Environmental Data")
+    process_sensor = SensorStream(" SENSOR_001","")
+    process_transaction = TransactionStream("TRANS_001","")
     data_sensor: List[Any] = ["temp:22.5", "humidity:65", "pressure:1013"]
+    data_transaction = ["buy:100", "sell:150", "buy:75"]
     print("= CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
     print("\nInitializing Sensor Stream...")
     print(f"Stream ID: {process_sensor.stream_id}, Type: {process_sensor.stream_type}")
     print("Processing sensor batch: [temp:22.5, humidity:65, pressure:1013]")
     print("Sensor analysis:",process_sensor.process_batch(data_sensor))
+    print("\nInitializing Transaction Stream...")
+    print(f"Stream ID: {process_transaction.stream_id}, Type: {process_transaction.stream_type}")
+    print("Processing sensor batch: [buy:100, sell:150, buy:75]")
+    print("Sensor analysis:",process_transaction.process_batch(data_transaction))
