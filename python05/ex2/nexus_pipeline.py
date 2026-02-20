e5b9c9786186e4e373b57d3e9c2a5ecc27418bad
Mohammed Hicham Benhabib <mobenhab@z4r9p4.42lyon.fr>
2026-02-20 08:45:32 +0100

    py05 ex2

--- python05/ex2/nexus_pipeline.py
+++ python05/ex2/nexus_pipeline.py
@@ -1,2 +1,31 @@
-import typing
+from typing import Any, List, Protocol
+from abc import ABC, abstractmethod
 
+
+class ProcessingStage(Protocol):
+    pass
+
+
+class ProcessingPipeline(ABC):
+    def __init__(self, pipeline_id: str) -> None:
+        self.pipeline_id: str = pipeline_id
+        self.stages: List[Any] = []
+        self.stats = {
+            "runs": 0,
+            "errors": 0,
+            "total_time": 0
+        }
+
+    @abstractmethod
+    def process(self, data: Any) -> Any:
+        pass
+
+class JSONAdapter(ProcessingPipeline):
+    pass
+
+class CSVAdapter(ProcessingPipeline):
+    pass
+
+class StreamAdapter(ProcessingPipeline):
+    def __init__(self) -> None:
+        super().__init__()
